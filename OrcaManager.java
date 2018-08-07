68fe85a4710057242b34b72522ba0d239f4c98f7
hexadec <10330938+hexadec@users.noreply.github.com>
2018-08-07 17:52:49 +0200

    Create OrcaManager.java

---
+++ OrcaManager.java
@@ -0,0 +1,87 @@
+package hu.hexadec.killerwhale;
+
+import android.content.ActivityNotFoundException;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Build;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+public final class OrcaManager {
+
+    private Context context;
+    private List<Orca> orcas;
+
+    public OrcaManager(Context context) throws FileNotFoundException {
+        this(context, null);
+    }
+
+    public OrcaManager(Context context, String[] excludeVendor) throws FileNotFoundException {
+        this.context = context;
+        try {
+            BufferedReader reader = new BufferedReader(
+                    new InputStreamReader(context.getAssets().open("orcas.csv"), "UTF-8"));
+            orcas = new ArrayList<>();
+            String line;
+            readerloop: while ((line = reader.readLine()) != null) {
+                String[] elements = line.split(",");
+                Orca o = new Orca();
+                o.settings = new ComponentName(elements[0],elements[1]);
+                o.vendor = elements[4];
+                if (excludeVendor != null) {
+                    for (String vendor : excludeVendor) {
+                        if (o.vendor.equalsIgnoreCase(vendor))
+                            continue readerloop;
+                    }
+                }
+                o.MinAPI = Integer.valueOf(elements[2]);
+                o.MaxAPI = Integer.valueOf(elements[3]);
+                orcas.add(o);
+            }
+        } catch (IOException e) {
+            throw new FileNotFoundException();
+        } catch (NumberFormatException e) {
+            throw new FileNotFoundException("Invalid file!");
+        } catch (ArrayIndexOutOfBoundsException e) {
+            throw new FileNotFoundException("Invalid file!");
+        }
+    }
+
+    public boolean hasVendorOptimization() {
+        for (Orca o : orcas) {
+            if (o.vendor.equalsIgnoreCase(Build.MANUFACTURER) &&
+                    o.MinAPI < Build.VERSION.SDK_INT && (o.MaxAPI > Build.VERSION.SDK_INT || o.MaxAPI == 0)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public void startIntents() throws ActivityNotFoundException {
+        context.startActivities(getIntents());
+    }
+
+    public Intent[] getIntents() {
+        List<Intent> intentList = new ArrayList<>();
+        for (Orca o : orcas) {
+            if (o.vendor.equalsIgnoreCase(Build.MANUFACTURER) &&
+                    o.MinAPI < Build.VERSION.SDK_INT && (o.MaxAPI > Build.VERSION.SDK_INT || o.MaxAPI == 0)) {
+                intentList.add(new Intent().setComponent(o.settings));
+            }
+        }
+        return intentList.toArray(new Intent[intentList.size()]);
+    }
+}
+
+final class Orca {
+    int MinAPI;
+    int MaxAPI;
+    String vendor;
+    ComponentName settings;
+}
